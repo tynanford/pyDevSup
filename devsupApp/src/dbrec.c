a72afde99aa0b6cf864dc8db1cfaa298f76691b1
Aqeel AlShafei <aqeel.alshafei@stfc.ac.uk>
2026-08-04 11:16:34 +0100

    Change message to amsg with update PyMethodDef documentation

--- devsupApp/src/dbrec.c
+++ devsupApp/src/dbrec.c
@@ -125,11 +125,11 @@ static PyObject* pyRecord_setSevr(pyRecord *self, PyObject *args, PyObject *kws)
 {
     dbCommon *prec = self->entry.precnode->precord;
 
-    static char* names[] = {"sevr", "stat", "message", NULL};
+    static char* names[] = {"sevr", "stat", "amsg", NULL};
     short sevr = INVALID_ALARM, stat=COMM_ALARM;
-    const char *message = NULL;
+    const char *amsg = NULL;
 
-    if(!PyArg_ParseTupleAndKeywords(args, kws, "|hhz", names, &sevr, &stat, &message))
+    if(!PyArg_ParseTupleAndKeywords(args, kws, "|hhz", names, &sevr, &stat, &amsg))
         return NULL;
 
     if(sevr<firstEpicsAlarmSev || sevr>lastEpicsAlarmSev
@@ -140,8 +140,8 @@ static PyObject* pyRecord_setSevr(pyRecord *self, PyObject *args, PyObject *kws)
     }
 // @since 7.0.6
 #ifdef HAS_ALARM_MESSAGE
-    if(message) {
-        recGblSetSevrMsg(prec, stat, sevr, "%s", message);
+    if(amsg) {
+        recGblSetSevrMsg(prec, stat, sevr, "%s", amsg);
         Py_RETURN_NONE;
     }
 #endif
@@ -324,8 +324,9 @@ static PyMethodDef pyRecord_methods[] = {
      "infos() -> {'name':'value'}\n"
      "Return a dictionary of all infos for this record."},
     {"setSevr", (PyCFunction)pyRecord_setSevr, METH_VARARGS|METH_KEYWORDS,
-     "setSevr(sevr=INVALID_ALARM, stat=COMM_ALARM)\n"
-     "Set alarm new alarm severity/status.  Record must be locked!"},
+     "setSevr(sevr=INVALID_ALARM, stat=COMM_ALARM, amsg=Nome)\n"
+     "Set alarm new alarm severity/status.  Record must be locked!\n"
+     "amsg requires EPICS Base >= 7.0.6."},
     {"setTime", (PyCFunction)pyRecord_setTime, METH_VARARGS,
      "Set record timestamp if TSE==-2.  Record must be locked!"},
     {"scan", (PyCFunction)pyRecord_scan, METH_VARARGS|METH_KEYWORDS,
